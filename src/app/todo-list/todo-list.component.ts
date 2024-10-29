0bcbe0c71597b87aa0e8c01c54e48e1117c5717a
mackowiakm <maksio.piksioo@gmail.com>
2024-10-29 11:41:53 +0100

    Update todo-list-component.html

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
+import {KeyValuePipe, NgForOf, NgIf} from '@angular/common';
+import {ToDo} from '../shared/interfaces/todo.interface';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [
+    KeyValuePipe,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-  todos : string[] = [];
+  todos : ToDo[] = [];
 
   addTodo(todo : string) : void {
-    this.todos.push(todo);
+    this.todos.push({name : todo , isComplete: false});
     console.log("Aktualna lista todo: ", this.todos);
+  }
+  changeDone(i : number){
+    this.todos[i].isComplete = !this.todos[i].isComplete;
+  }
+  removeTodo(i : number){
+    this.todos.splice(i, 1);
   }
 }
